c37bc171b4cccc6058afc42cd514b9f93ef84f97
Nick <nickdloucks@outlook.com>
2021-09-30 08:34:37 -0700

    styling started in Tag, del redundant prof card

--- src/ProfileCard.js
+++
@@ -1,6 +0,0 @@
-function ProfileCard(){
-
-}
-
-
-export default ProfileCard;
--- src/components/TeamTag.tsx
+++ src/components/TeamTag.tsx
@@ -5,11 +5,17 @@ interface NameProps {
   logo: string; // URL for the team's SVG logo
 }
 
+const style: object = {
+  // figure out how to select by a dynamically generated ID
+}
+
 const TeamTag: FC<NameProps> = ({ teamName }) => {
   return (
     <>
-      <div>{/*DISPLAY logo here, not just URL string*/}</div>
-      <div>{teamName}</div>
+      <div id = {`${teamName}-tag`}>
+        <div>{/*DISPLAY logo here, not just URL string*/}</div>
+        <div>{teamName}</div>
+      </div>
     </>
   );
 };
