2bbee7a3f8264c1bbb9e12ec80616bf5e58d877d
Nick <nickdloucks@outlook.com>
2021-11-02 08:08:22 -0700

    starded skeleton of standings component

---
+++ src/confStandings.ts
@@ -0,0 +1,12 @@
+import React from'react';
+import {default as Team} from './TeamClass';
+
+const Standings = function(teamList: Team[], groupId: string){ // group ID will be a string to identify the conference, league, etc.
+    const rows = teamList.map(function(team: Team){
+        return(
+            
+        );
+    });
+}
+
+export default Standings;
