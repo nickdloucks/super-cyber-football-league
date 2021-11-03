250e10874ef046610579bd3f749cd45671cacecc
Nick <nickdloucks@outlook.com>
2021-11-03 07:52:28 -0700

    change extention to 'tsx' so JSX will work

--- src/confStandings.tsx
+++ src/confStandings.tsx
@@ -4,7 +4,11 @@ import {default as Team} from './TeamClass';
 const Standings = function(teamList: Team[], groupId: string){ // group ID will be a string to identify the conference, league, etc.
     const rows = teamList.map(function(team: Team){
         return(
-            
+            <>
+                <tr>
+                    <td></td>
+                </tr>
+            </>
         );
     });
 }
