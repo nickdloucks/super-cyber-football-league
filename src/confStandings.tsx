121daf1945ade7b1b82aa797188afbec1c432d36
Nick <nickdloucks@outlook.com>
2021-11-03 08:37:22 -0700

    started column formatting for standings

--- src/confStandings.tsx
+++ src/confStandings.tsx
@@ -3,6 +3,8 @@ import {default as Team} from './TeamClass';
 
 const Standings = function(teamList: Team[], groupId: string){ // group ID will be a string to identify the conference, league, etc.
     const rows = teamList.map(function(team: Team){
+        //let winPct = team.record.wins/({team.record.wins} + {team.record.losses} + {team.record.ties});
+        //Number.parseFloat(winPct).toFixed(3);
         return(
             <>
                 <tr key={team.profile.name.concat('-standingsRow')}>
@@ -11,6 +13,7 @@ const Standings = function(teamList: Team[], groupId: string){ // group ID will 
                     <td>{team.record.wins}</td>
                     <td>{team.record.losses}</td>
                     <td>{team.record.ties}</td>
+                    <td>{/* INSERT WIN % HERE */}</td>
                 </tr>
             </>
         );
@@ -18,9 +21,17 @@ const Standings = function(teamList: Team[], groupId: string){ // group ID will 
 
     return(
         <>
+            <span>{groupId} Standings</span>
             <table>
                 <thead>
-                    {groupId} Standings
+                    <tr>
+                        <th>{/*logo*/}</th>
+                        <th scope='col'>Team</th>
+                        <th scope='col'>Wins</th>
+                        <th scope='col'>Losses</th>
+                        <th scope='col'>Ties</th>
+                        <th scope='col'>Pct.</th>
+                    </tr>
                 </thead>
                 <tbody>
                     {rows}
