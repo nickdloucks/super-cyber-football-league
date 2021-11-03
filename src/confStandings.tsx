2417f73509a0870b7a69a087e7280fad2f307085
Nick <nickdloucks@outlook.com>
2021-11-03 08:21:50 -0700

    started outlining data for table cells

--- src/confStandings.tsx
+++ src/confStandings.tsx
@@ -5,12 +5,29 @@ const Standings = function(teamList: Team[], groupId: string){ // group ID will 
     const rows = teamList.map(function(team: Team){
         return(
             <>
-                <tr>
-                    <td></td>
+                <tr key={team.profile.name.concat('-standingsRow')}>
+                    <th scope='row'>{/* INSERT LOGO CARD HERE */}</th>
+                    <td>{team.profile.name}</td>
+                    <td>{team.record.wins}</td>
+                    <td>{team.record.losses}</td>
+                    <td>{team.record.ties}</td>
                 </tr>
             </>
         );
     });
+
+    return(
+        <>
+            <table>
+                <thead>
+                    {groupId} Standings
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        </>
+    );
 }
 
 export default Standings;
