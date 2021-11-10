9035db42035017b19fdcce7fbb74d56487c5cce8
Nick <nickdloucks@outlook.com>
2021-11-10 12:40:09 -0800

    schedule component; initial commit

---
+++ src/components/scheduleTable.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {default as TeamTag} from "./TeamTag";
+import {default as Team} from "../TeamClass";
+
+const Schedule = function(subjectTeam: Team){
+    let opponents = new Array(7); // create new array of games to be filled with opponents later. 
+    // set default value to the string 'empty' instead of <undefined>
+    return(
+        <>
+            <table>
+                
+            </table>
+        </>
+    );
+}
