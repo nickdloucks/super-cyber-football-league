a464e450791efbca57a8b3f4ca727a9672fe29ec
Nick <nickdloucks@outlook.com>
2021-11-11 17:06:53 -0800

    setup documentation and params for SIM

---
+++ src/GameSim.ts
@@ -0,0 +1,13 @@
+import {default as Team } from './TeamClass';
+
+/**
+ * This function will be one of the main tasks that the SCFL app performs.
+ * Calling this function will use roster data to simulate a football game between the two teams (parameters).
+ * It will update the stats for each team, and provide a final score/game result.
+ * Input the two teams that will be playing the game: one home team, one away team.
+ * 
+ */
+
+const SIMULATE_GAME = function(homeTeam: Team, awayTeam: Team){
+
+}
