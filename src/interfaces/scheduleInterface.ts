8eda480de4bbe42b02b6538026c0e0d465c71abb
Nick <nickdloucks@outlook.com>
2021-11-11 17:19:01 -0800

    documentation

--- src/interfaces/scheduleInterface.ts
+++ src/interfaces/scheduleInterface.ts
@@ -1,3 +1,12 @@
+/**
+ * As of the 3002 season, he SCFL plays a 7 game regular season, preceded by a league-wide tournament.
+ * Those teams with good enough regular season records are invited to the playoffs for 
+ * single-elimination Bowl-games, eventually crowning the leagues Champion for the year. 
+ * This interface will be the structure for each team to store
+ * a record of the games it plays in one year. 
+ * It's broken down into pre-season, regular season, and, if applicable, post-season.
+ */
+
 export interface TeamSchedule {
     scft: Array<object>,
     regSeason: Array<object>,
