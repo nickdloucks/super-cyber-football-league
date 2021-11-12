67a46a3b5940f55375771a822913df600c374645
Nick <nickdloucks@outlook.com>
2021-11-11 17:27:55 -0800

    outline map(opponents) table data

--- src/components/scheduleTable.tsx
+++ src/components/scheduleTable.tsx
@@ -3,12 +3,17 @@ import {default as TeamTag} from "./TeamTag";
 import {default as Team} from "../TeamClass";
 
 const Schedule = function(subjectTeam: Team){
-    let opponents = new Array(7); // create new array of games to be filled with opponents later. 
+    //let opponents = new Array(7); // create new array of games to be filled with opponents later. 
     // set default value to the string 'empty' instead of <undefined>
+    let opponents = subjectTeam.schedule.regSeason.map(opponent:Team => <tr><td>{opponent.name}</td><td>{'insert score here'}</td></tr>);
     return(
         <>
             <table>
-                
+                <thead>
+                    <td>Opponent:</td>
+                    <td>Score:</td>
+                </thead>
+                {opponents}
             </table>
         </>
     );
