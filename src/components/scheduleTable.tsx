4385a440d9e7381a797dbad22ae162c9c85b5383
Nick <nickdloucks@outlook.com>
2021-11-12 17:41:18 -0800

    documentation

--- src/components/scheduleTable.tsx
+++ src/components/scheduleTable.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import {default as TeamTag} from "./TeamTag";
 import {default as Team} from "../TeamClass";
 
+/**
+ * 
+ * @param subjectTeam: the team whose schedule this will display
+ * @returns Table of opponent teams and the score of the game played between the two
+ * note* configure such that the schedule table can display pre-season, regular season, or postseason (or all three)
+ */
+
 const Schedule = function(subjectTeam: Team){
     //let opponents = new Array(7); // create new array of games to be filled with opponents later. 
     // set default value to the string 'empty' instead of <undefined>
-    let opponents = subjectTeam.schedule.regSeason.map(opponent:Team => <tr><td>{opponent.name}</td><td>{'insert score here'}</td></tr>);
+    // let opponents = subjectTeam.schedule.regSeason.map(opponent:Team => <tr><td>{opponent.name}</td><td>{'insert score here'}</td></tr>);
     return(
         <>
             <table>
@@ -13,7 +20,7 @@ const Schedule = function(subjectTeam: Team){
                     <td>Opponent:</td>
                     <td>Score:</td>
                 </thead>
-                {opponents}
+                {/*opponents*/}
             </table>
         </>
     );
