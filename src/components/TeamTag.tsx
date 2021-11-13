6e8492b212654e1a01a86174b904e641f20c5248
Nick <nickdloucks@outlook.com>
2021-11-12 17:53:38 -0800

    documentation

--- src/components/TeamTag.tsx
+++ src/components/TeamTag.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from 'react'; // React Functional Component
 
+/**
+ * The team tag is a custom, stylized element that will be used to display a given team's logo and name.
+ * It will most frequently be used for standings tables, schedule tables, and at the top of a game log document.
+ */
+
+
 interface NameProps {
   teamName: string; // the team's name
   logo: string; // URL for the team's SVG logo
