526490ef1764ede232a68de0367b47f4b53a59b6
Nick <nickdloucks@outlook.com>
2021-11-20 19:06:16 -0800

    add opponents array param to schedule function

--- src/components/scheduleTable.tsx
+++ src/components/scheduleTable.tsx
@@ -9,7 +9,8 @@ import {default as Team} from "../TeamClass";
  * note* configure such that the schedule table can display pre-season, regular season, or postseason (or all three)
  */
 
-const Schedule = function(subjectTeam: Team){
+const Schedule = function(subjectTeam: Team, opponents:Array<Team>){ //  SEPARATE OPPONENTS ARRAY? 
+    //OR WILL THIS BE STORED ON THE SUBJECT TEAM ITSELF?
     //let opponents = new Array(7); // create new array of games to be filled with opponents later. 
     // set default value to the string 'empty' instead of <undefined>
     // let opponents = subjectTeam.schedule.regSeason.map(opponent:Team => <tr><td>{opponent.name}</td><td>{'insert score here'}</td></tr>);
