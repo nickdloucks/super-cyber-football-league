bd0becad7227505fbc9c905d9f1e0662de1ec6a4
Nick <nickdloucks@outlook.com>
2021-11-27 16:45:39 -0800

    playoffs initial commit

---
+++ src/components/playoffs.tsx
@@ -0,0 +1,18 @@
+import Team from "../TeamClass";
+
+function PlayoffsBracket(): JSX.Element{
+    return(
+        <>
+            <h1>Playoffs</h1>
+            <div>
+                <h3>Anchor Bowl</h3>
+            </div>
+            <div>
+                <h3>Ax Bowl</h3>
+            </div>
+            <div>
+                <h2>Sock Bowl</h2>
+            </div>
+        </>
+    );
+}
