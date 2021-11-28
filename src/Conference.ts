03c5d131970363646c9f6592dc3781dbc6879229
Nick <nickdloucks@outlook.com>
2021-11-27 18:17:11 -0800

    conference class initial commit

---
+++ src/Conference.ts
@@ -0,0 +1,19 @@
+import Team from "./TeamClass";
+
+class Conference {
+    name: string;
+    abrv: string;
+    teams: Array<Team>;
+    bowl: string;
+
+    constructor(conf_name:string, conf_abrv:string, conf_teams:Array<Team>, conf_bowl:string) {
+        this.name: conf_name,
+        this.abrv: conf_abrv,
+        this.teams: conf_teams,
+        this.bowl: conf_bowl
+    }
+    
+
+}
+
+export default Conference;
