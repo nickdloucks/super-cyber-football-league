1c57cff146ae423bfefc0e5301ef676b513133bc
Nick <nickdloucks@outlook.com>
2021-11-28 19:43:52 -0800

    outlining conf standings sort callback

--- src/Conference.ts
+++ src/Conference.ts
@@ -5,6 +5,9 @@ import Team from "./TeamClass";
  * The Conference class is used to define a group of Teams. 
  * It is below the League level (meaning there are multiple conferences), but above the Division level
  * (a conference can have multiple Divisions).
+ * 
+ * The Conference's <currentStandings> method will return the list of teams in the conference 
+ * sorted by highest winning percentage first.
  */
 
 class Conference {
@@ -12,14 +15,20 @@ class Conference {
     abrv: string;
     teams: Array<Team>;
     bowl: string;
+    currentStandings(): Array<Team>{
+        return this.teams.sort(function (team_a:Team, team_b:Team){
+            return team_b.getWinPct() > team_a.getWinPct();
+        });
+    };
 
     constructor(conf_name:string, conf_abrv:string, conf_teams:Array<Team>, conf_bowl:string) {
         this.name = conf_name;
         this.abrv = conf_abrv;
         this.teams = conf_teams;
         this.bowl = conf_bowl;
+        this.currentStandings = this.currentStandings.bind(this);
     }
-    
+
 
 }
 
