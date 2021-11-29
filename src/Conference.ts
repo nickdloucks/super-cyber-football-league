c020c6433ffa87ec1ed5cab1972ea24697ecc6b9
Nick <nickdloucks@outlook.com>
2021-11-28 16:50:53 -0800

    fixed syntax error in constructor

--- src/Conference.ts
+++ src/Conference.ts
@@ -7,10 +7,10 @@ class Conference {
     bowl: string;
 
     constructor(conf_name:string, conf_abrv:string, conf_teams:Array<Team>, conf_bowl:string) {
-        this.name: conf_name,
-        this.abrv: conf_abrv,
-        this.teams: conf_teams,
-        this.bowl: conf_bowl
+        this.name = conf_name;
+        this.abrv = conf_abrv;
+        this.teams = conf_teams;
+        this.bowl = conf_bowl;
     }
     
 
