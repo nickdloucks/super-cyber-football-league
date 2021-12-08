3e1353fc19eee6dce613bf508df310b4867d281c
Nick <nickdloucks@outlook.com>
2021-12-07 20:41:04 -0800

    comment out team creator so it will compile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">SCFL App</header>
-      <TeamCreator />
+      {/*<TeamCreator />*/}
       {/*<Standings />*/}
     </div>
   );
