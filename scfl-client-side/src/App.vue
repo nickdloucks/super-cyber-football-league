88c9d4a3de5723f4373e25140bb953a75c0aedbc
Nick <nickdloucks@outlook.com>
2023-05-06 15:59:56 -0700

    ref stylesheet, ignore dev-notes in tsconfig

--- scfl-client-side/src/App.vue
+++ scfl-client-side/src/App.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue'
   // import StandingsTable from './components/StandingsTable.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <StandingsTable/>
+  <!-- <StandingsTable/> -->
 </template>
 
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -28,5 +18,5 @@
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
