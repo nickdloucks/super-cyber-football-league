cce0abd049cd4cbf5666765e86931dd41df1ae4d
Nick <nickdloucks@outlook.com>
2023-05-07 17:36:48 -0700

    init standings table

---
+++ scfl-client-side/src/components/StandingsTable.vue
@@ -0,0 +1,103 @@
+
+<script lang="ts">
+    import type {TeamDisplay} from "../types/team"
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+        name: "StandingsTable",
+        props: {
+            items: {
+                type: Array<TeamDisplay>,
+                required: true,
+            },
+            tableHeader: {
+                type: String,
+                required: true,
+            },
+        },
+    });
+</script>
+
+<!-- let teams: Array<TeamDisplay> = [
+    // {"team_name", "conference", "color_1", "color_2", "color_3", "color_4"}
+    {team_name: "Bats", conference: "NC", color_1: "#4b0082", color_2: "#818181", color_3: "#000", color_4:"#1f1f1f"},
+    {team_name: "Yetis", conference: "NC", color_1: "", color_2: "", color_3: "", color_4: ""},
+    {team_name: "Flyingfish", conference: "NC", color_1: "", color_2: "", color_3: "", color_4: ""},
+    {team_name: "Sharks", conference: "NC", color_1: "", color_2: "", color_3: "", color_4: ""},
+    {team_name: "Zephyrs", conference: "NC", color_1: "", color_2: "", color_3: "", color_4: ""}
+] -->
+
+/////////////////////////////////////////////////////////////////
+<template>
+  <div class="table-container">
+    <table class="table">
+      <thead>
+        <tr>
+          <th colspan="8">{{ tableHeader }}</th>
+        </tr>
+        <tr>
+          <!-- <th>Logo</th> -->
+          <th>Team</th>
+          <th>Conference</th>
+          <th>Color 1</th>
+          <th>Color 2</th>
+          <th>Color 3</th>
+          <th>Color 4</th>
+          <!-- <th>Record</th>
+          <th>Win Pct.</th>
+          <th>Conference</th>
+          <th>Conf. Pct.</th>
+          <th>SOS</th>
+          <th>SOV</th> -->
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index">
+          <!-- <td>{{ item.logo }}</td> -->
+          <td>{{ item.team_name }}</td>
+          <td>{{ item.conference }}</td>
+          <td :background-color="item.color_1">{{ item.color_1 }}</td>
+          <td>{{ item.color_2 }}</td>
+          <td>{{ item.color_3 }}</td>
+          <td>{{ item.color_4 }}</td>
+          <!-- <td>{{ item.record }}</td>
+          <td>{{ item.winPct }}</td>
+          <td>{{ item.conference }}</td>
+          <td>{{ item.confPct }}</td>
+          <td>{{ item.sos }}</td>
+          <td>{{ item.sov }}</td> -->
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+
+.table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th,tr {
+  border: 3px solid #000;
+  padding: 8px;
+}
+
+th{
+    min-width: max-content;
+}
+
+th {
+  text-align: center;
+  background-color: #444;
+}
+
+tbody:nth-child(-n + 2) {
+  border-bottom: 2px dotted #fff;
+}
+
+.table-container {
+  overflow-x: auto;
+}
+</style>
